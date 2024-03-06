d60968e0882aa8d4ff9bbf93a1a51d41e3aff760
Emily T <tejedae1@uci.edu>
2024-03-05 22:13:16 -0800

    Initial commit

---
+++ js/intro.js
@@ -0,0 +1,20 @@
+//resource link: https://www.w3schools.com/jquery/default.asp
+
+$(document).ready(function(){
+    $("#wand").show();
+    $("#wand2").hide();
+
+
+    $("#wand").click(function(){
+        $("#wand2").show();
+        $("#wand").hide();
+    });
+
+    
+
+
+   
+
+    
+    });
+
