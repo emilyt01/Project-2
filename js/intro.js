5dcf2aea8f1f3ada8a98e81cb94ea9fcafe8cb31
Emily T <tejedae1@uci.edu>
2024-03-10 21:59:49 -0700

    update index.html

--- js/intro.js
+++ js/intro.js
@@ -3,17 +3,37 @@
 $(document).ready(function(){
     $("#wand").show();
     $("#wand2").hide();
-
+    $("#mirror").show();
+    $("#text").hide();
+    $("#text2").show();
+    $("#text5").hide();
 
     $("#wand").click(function(){
         $("#wand2").show();
         $("#wand").hide();
     });
 
-    
+    $("#text3").click(function(){
+        $("#text").toggle();
+        $("#text3").hide();
+        $("#text2").hide();
+    });
 
+    $("#text").click(function(){
+        $("#text3").toggle();
+        $("#text2").show();
+        $("#text").hide();
+    });
 
+    $("#gif").click(function(){
+        $("#text5").toggle();
+        $("#gif").hide();
+    });
    
+    $("#text5").click(function(){
+        $("#gif").toggle();
+        $("#text5").hide();
+    })
 
     
     });
